3403f699f7e9e03d0ae09c641a418de48f2a6c5e
Chinh23 <119948744+Chinh23-11-03-03@users.noreply.github.com>
2024-04-23 11:32:32 +0700

    Update MainTest.java

--- MainTest.java
+++ MainTest.java
@@ -9,20 +9,12 @@ public class MainTest {
         int n = 5;
         int expectedSum = 15;
 
-        int actualSum = Main.calculateSumOfPositiveIntegers(n);
+        int actualSum = Main.calculateSumOfPositiveIntegers(n);//
 
         assertEquals(expectedSum, actualSum);
     }
 
-    @Test
-    public void testEvenSum() {
-        int n = 10;
-
-        boolean isEvenSum = Main.isEvenSum(Main.calculateSumOfPositiveIntegers(n));
-
-        assertTrue(isEvenSum);
-    }
-
+  
     @Test
     public void testOddSum() {
         int n = 9;
