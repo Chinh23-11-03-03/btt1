1aa1987523a7419b16eeb29456c98bca4fcd6248
Chinh23 <119948744+Chinh23-11-03-03@users.noreply.github.com>
2024-04-23 11:35:16 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
 
-public class Main { // Semicolon added
+public class Main {
 
     public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-  
-      System.out.print("Nhap so nguyen duong: ");
-      int n = scanner.nextInt();
-  
-      int sum = 0;
-      for (int i = 1; i <= n; i++) {
-        sum += i;
-      }
-  
-      if (sum % 2 == 0) {
-        System.out.println("Tong la so chan.");
-      } else {
-        System.out.println("Tong la so le.");
-      }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("nhap so nguyen duong: ");
+        int n = scanner.nextInt();
+
+        int sum = calculateSumOfPositiveIntegers(n); // Gọi phương thức đã được triển khai
+
+        if (sum % 2 == 0) {
+            System.out.println("Tong la so chan.");
+        } else {
+            System.out.println("Tong la so le.");
+        }
     }
 
-   public static int calculateSumOfPositiveIntegers(int n) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'calculateSumOfPositiveIntegers'");
-	}
-
-    public static boolean isOddSum(int calculateSumOfPositiveIntegers) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isOddSum'");
+    public static int calculateSumOfPositiveIntegers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
-  }
-  
+
+    public static boolean isEvenSum(int sum) {
+        return sum % 2 == 0;
+    }
+
+    public static boolean isOddSum(int sum) {
+        return !isEvenSum(sum); // Hoặc return sum % 2 != 0;
+    }
+}
